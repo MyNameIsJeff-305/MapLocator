981cc5af9f11949cf44c4e5712c12acf8487512f
Michel <michelgarcia950528@icloud.com>
2024-06-19 16:39:35 -0400

    Routes Setup

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const routes = require('./routes')
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -14,6 +16,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
+app.use(routes);
 
 //Security Middleware
 if(!isProduction) {
@@ -35,3 +38,5 @@ app.use(
         }
     })
 );
+
+module.exports = app;
