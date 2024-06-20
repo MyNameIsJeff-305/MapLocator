d5b5ad0c013204f0a2c21912a254dad0ef3ad1d5
Michel <michelgarcia950528@icloud.com>
2024-06-20 13:02:41 -0400

    Spots Model and Migration

---
+++ backend/db/models/spot.js
@@ -0,0 +1,96 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+
+    static associate(models) {
+      Spot.belongsTo(
+        models.User, {
+        foreignKey: 'createdBy',
+      })
+    }
+  }
+  Spot.init({
+    createdBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 100]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 100]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 100]
+      }
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 30]
+      }
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: -90,
+        max: 90
+      }
+    },
+    lng: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: -180,
+        max: 180
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 100],
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [0, 500]
+      }
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        min: 0,
+        isFloat: true,
+        isNumeric: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
