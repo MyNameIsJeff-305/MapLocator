680076ad6269d8ff00ca134a9752efbeda3ebd4c
Michel <michelgarcia950528@icloud.com>
2024-06-20 11:18:17 -0400

    Add Users table migration and seeds

---
+++ backend/db/models/user.js
@@ -0,0 +1,61 @@
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  };
+
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          }
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 30]
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 50]
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
+      }
+    }, {
+    sequelize,
+    modelName: 'User'
+  }
+  );
+  return User;
+};
---
+++ backend/db/seeders/01-demo-user.js
@@ -0,0 +1,29 @@
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'test@user.io',
+        username: 'testing',
+        firstName: 'Testing',
+        lastName: 'User',
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], { validate: true });
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['testing'] }
+    }, {});
+  }
+};
