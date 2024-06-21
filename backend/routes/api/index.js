be97490dadc76cd5fb0aae413e052e7fe8aafe4b
Michel <michelgarcia950528@icloud.com>
2024-06-21 11:03:38 -0400

    Spots Router Setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,13 +2,16 @@ const router = require("express").Router();
 
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
 
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/spots', spotsRouter);
 
+//Testing Route
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
